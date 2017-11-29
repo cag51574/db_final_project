7be18101b3588fe54116f2d2100e9acdd24c1fb5
Colin Wahl <colin.t.wahl@gmail.com>
2017-11-29 15:40:31 -0500

    restaurant and indiviudal restaurant endpoints

--- backend/app/controllers/RestaurantController.java
+++ backend/app/controllers/RestaurantController.java
@@ -14,29 +14,38 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // Our Imports
-import models.Restaurant;
 import repository.RestaurantRepository;
-import repository.InventoryRepository;
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class HomeController extends Controller {
+public class RestaurantController extends Controller {
 
     private final RestaurantRepository restaurantRepository;
-    private final InventoryRepository inventoryRepository;
     private final HttpExecutionContext httpExecutionContext;
 
     @Inject
-    public HomeController(RestaurantRepository restaurantRepository,
-                          InventoryRepository inventoryRepository,
+    public RestaurantController(RestaurantRepository restaurantRepository,
                           HttpExecutionContext httpExecutionContext) {
         this.restaurantRepository = restaurantRepository;
-        this.inventoryRepository = inventoryRepository;
         this.httpExecutionContext = httpExecutionContext;
     }
 
-   
+    public Result restaurants() {
+        return ok(Json.toJson(restaurantRepository.all()));
+    }
+
+    public Result restaurant(String name) {
+        return ok(Json.toJson(restaurantRepository.byName(name)));
+    }
+
+    
+    /*
+    public CompletionStage<Result> restaurants() {
+        return restaurantRepository.all().thenApplyAsync(list -> {
+                return ok(Json.toJson(list));
+            }, httpExecutionContext.current());
+    }
 
     public CompletionStage<Result> restaurantNames() {
         return restaurantRepository.allNames().thenApplyAsync(list -> ok(Json.toJson(list)), httpExecutionContext.current());
@@ -45,15 +54,15 @@ public class HomeController extends Controller {
     public CompletionStage<Result> inventories() {
         return inventoryRepository.all().thenApplyAsync(list -> ok(Json.toJson(list)), httpExecutionContext.current());
     }
-
+    */
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
-     */
+     *
     public Result index() {
         return ok("Your new application is ready.");
     }
-
+    */
 }
