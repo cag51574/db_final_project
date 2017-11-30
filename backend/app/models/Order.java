55e4e1036ce12fba8c28b0d9349b867a8f958996
Colin Wahl <colin.t.wahl@gmail.com>
2017-11-29 23:34:30 -0500

    add orders and tickets

--- backend/app/models/Order.java
+++ backend/app/models/Order.java
@@ -6,16 +6,18 @@ import io.ebean.Model;
 import play.data.validation.Constraints;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 // Restaurant entity which is managed by Ebean.
 @Entity
-public class Item_Order extends Model {
+@Table(name="Item_Order")
+public class Order extends Model {
 
        public int order_num;
 
        public String item_name;
 
        public String restaurant_name;
-       
+
        public int item_quantity;
 }
